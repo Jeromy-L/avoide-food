449d278af50a9d4deb032e7e54d975fb167b1d57
Jeromy <843755624@qq.com>
2016-09-20 00:12:10 +0800

    绝壁不改了

--- index.js
+++ index.js
@@ -13,15 +13,12 @@
         btn.style.display="none";
         footer.style.display="none";
         body.style.background="";
-        // document.body.style.background="url(bg1.jpg)";
-        // document.body.style.background="100% 100%"
         body.setAttribute('class',"back");
         game();
     }
     left.onclick=function(){
         confirm.style.display="none";
         share.style.display="none";
-        //document.title = '你能逃离北二的控制吗';
         var title = '你能逃离北二的控制吗';
         changeTitle(title);
         game();
@@ -49,6 +46,9 @@
         canvas.height = ch;
         var starImg = document.getElementById("emoji");
         var rice = document.getElementById("rice");
+        var tomato = document.getElementById("tomato");
+        var eggplant = document.getElementById("eggplant");
+        var corn = document.getElementById("corn");
         var star = {
             x:cw/2,
             y:ch/2,
@@ -87,6 +87,7 @@
             var item = {
                 x:0,
                 y:0,
+                num:0,
                 speedX:0,
                 speedY:0,
                 covar:Math.random()+0.2   //自定义随机速度系数，让球类的速度不一样
@@ -95,18 +96,22 @@
                 case 0:
                     item.x = 0;
                     item.y = Math.random()*ch;
+                    item.num = 0;
                     break;
                 case 1:
                     item.x = Math.random()*cw;
                     item.y = 0;
+                    item.num = 1;
                     break;
                 case 2:
                     item.x = cw;
                     item.y = Math.random()*ch;
+                    item.num = 2;
                     break;
                 case 3:
                     item.x = Math.random()*cw;
                     item.y = ch;
+                    item.num = 3;
                     break;
                 default:
                     break;
@@ -175,7 +180,21 @@
                 ctx.translate(item.x,item.y);  //重置起始点坐标为星星中心点
                 ctx.rotate(item.time*Math.PI/180);  //旋转的角度
                 //ctx.beginPath();
-                ctx.drawImage(rice,-itemSize,-itemSize,itemSize*2,itemSize*2);
+                switch(item.num){
+                    case 0: 
+                        ctx.drawImage(rice,-itemSize,-itemSize,itemSize*2,itemSize*2);
+                        break;
+                    case 1:
+                        ctx.drawImage(eggplant,-itemSize,-itemSize,itemSize*2,itemSize*2);
+                        break;
+                    case 2:
+                        ctx.drawImage(tomato,-itemSize,-itemSize,itemSize*2,itemSize*2);
+                        break;
+                    case 3:
+                        ctx.drawImage(corn,-itemSize,-itemSize,itemSize*2,itemSize*2);
+                        break;
+                }
+                // ctx.drawImage(rice,-itemSize,-itemSize,itemSize*2,itemSize*2);
                 //ctx.closePath();
                 ctx.restore();
                 // ctx.beginPath();
@@ -219,114 +238,3 @@
             }
         },true);
     }
-
-
-     // wx.config({
-     //        debug: false,
-     //        appId: 'wx53f3924f225e6704',
-     //        timestamp: '1414587457',
-     //        nonceStr: 'Wm3WZYTPz0wzccnW',
-     //        signature: 'SHA1',
-     //        jsApiList: [
-     //            'checkJsApi',
-     //            'onMenuShareTimeline',
-     //            'onMenuShareAppMessage'
-     //        ]
-     //    });
-     //    wx.ready(function () {
-     //        // 2.1 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
-     //        wx.onMenuShareTimeline({
-     //            title: '我在逃离北二饭堂游戏中撑了'+(itemsCount-10)/5+'s',
-     //            link: window.location.href,
-     //            imgUrl: 'emoji.png',
-     //            trigger: function (res) {
-     //                // alert("分享到朋友圈按钮点击");        
-     //            },
-     //            success: function (res) {
-     //            },
-     //            cancel: function (res) {
-     //                // alert('已取消');
-     //            },
-     //            fail: function (res) {
-     //                alert(JSON.stringify(res));
-     //            }
-     //        });
-     //        // 2.2 监听“分享给朋友”按钮点击、自定义分享内容及分享结果接口
-
-     //        wx.onMenuShareAppMessage({
-     //            title: '我在逃离北二饭堂游戏中撑了'+(itemsCount-10)/5+'s',
-     //            link: window.location.href,
-     //            imgUrl: 'emoji.png',
-     //            success: function () { 
-     //                // 用户确认分享后执行的回调函数
-     //            },
-     //            cancel: function () { 
-     //                // 用户取消分享后执行的回调函数
-     //            }
-     //        });
-     //    });
-
-
-
-
-
-
-
-
-
-
-
-    // var imgUrl = "emoji.png";
-    // var lineLink = window.location.href;
-    // var descContent = '我在逃离北二饭堂游戏中撑了'+(itemsCount-10)/5+'s';
-    // var shareTitle = '神奇的北二饭堂';
-    // var appid = '';
-    
-    // function shareFriend() {
-    //     WeixinJSBridge.invoke('sendAppMessage',{
-    //         "appid": appid,
-    //         "img_url": imgUrl,
-    //         "img_width": "200",
-    //         "img_height": "200",
-    //         "link": lineLink,
-    //         "desc": descContent,
-    //         "title": shareTitle
-    //     }, function(res) {
-    //         //_report('send_msg', res.err_msg);
-    //     })
-    // }
-    // function shareTimeline() {
-    //     WeixinJSBridge.invoke('shareTimeline',{
-    //         "img_url": imgUrl,
-    //         "img_width": "200",
-    //         "img_height": "200",
-    //         "link": lineLink,
-    //         "desc": descContent,
-    //         "title": shareTitle
-    //     }, function(res) {
-    //            //_report('timeline', res.err_msg);
-    //     });
-    // }
-    // function shareWeibo() {
-    //     WeixinJSBridge.invoke('shareWeibo',{
-    //         "content": descContent,
-    //         "url": lineLink,
-    //     }, function(res) {
-    //         //_report('weibo', res.err_msg);
-    //     });
-    // }
-    // // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
-    // document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-    //     // 发送给好友
-    //     WeixinJSBridge.on('menu:share:appmessage', function(argv){
-    //         shareFriend();
-    //     });
-    //     // 分享到朋友圈
-    //     WeixinJSBridge.on('menu:share:timeline', function(argv){
-    //         shareTimeline();
-    //     });
-    //     // 分享到微博
-    //     WeixinJSBridge.on('menu:share:weibo', function(argv){
-    //         shareWeibo();
-    //     });
-    // }, true);
